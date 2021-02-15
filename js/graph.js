eb43c8087e266bc267ee8225a3edc54652187c79
J A MartinezRamirez <jorgemartinez05@gmail.com>
2021-02-15 16:43:31 +0100

    Add dashboard selection

--- js/graph.js
+++ js/graph.js
@@ -16,12 +16,19 @@ var svg = d3.select("#my_dataviz")
     .append("g")
     .attr("transform",
     "translate(" + margin.left + "," + margin.top + ")");
+var svgDiff = d3.select("#diff-plot")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform",
+    "translate(" + margin.left + "," + margin.top + ")");
 
 lastStep = 500; // Steps start at 0
 const fractionsW = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7];
 const segPreferences = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1];
 const repetitions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
-const groups = ['top20M', 'top20SegF', 'top20TestF']
+const groups = ['top20M', 'top20SegF', 'top20TestF'];
 var runs = {};
 
 fractionsW.forEach(fractionW => {
@@ -40,7 +47,11 @@ var myColor = d3.scaleOrdinal()
 var x = d3.scaleLinear()
     .domain([0, lastStep])
     .range([0, width]);
+
 svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x));
+svgDiff.append("g")
     .attr("transform", "translate(0," + height + ")")
     .call(d3.axisBottom(x));
 
@@ -50,6 +61,12 @@ var y = d3.scaleLinear()
     .range([height, 0]);
 svg.append("g")
     .call(d3.axisLeft(y));
+
+var yDiff = d3.scaleLinear()
+    .domain([-500, 3200])
+    .range([height, 0]);
+svgDiff.append("g")
+    .call(d3.axisLeft(yDiff));
 
 d3.select('.legend-color-men')
     .style('background-color', myColor('top20M'));
@@ -73,13 +90,18 @@ data.forEach(row => {
         step: row['[step]'],
         top20M: row['mean [rating] of max-n-of 20 turtles with [sex = "M"] [rating]'],
         top20SegF: row['mean [rating] of max-n-of 20 turtles with [group = "segregated"] [rating]'],
-        top20TestF: row['mean [rating] of max-n-of 20 turtles with [group = "test"] [rating]']
+        top20TestF: row['mean [rating] of max-n-of 20 turtles with [group = "test"] [rating]'],
+        MDiffSegF: row['mean [rating] of max-n-of 20 turtles with [sex = "M"] [rating]'] - row['mean [rating] of max-n-of 20 turtles with [group = "segregated"] [rating]'],
+        MDiffTestF: row['mean [rating] of max-n-of 20 turtles with [sex = "M"] [rating]'] - row['mean [rating] of max-n-of 20 turtles with [group = "test"] [rating]'],
+        TestFDiffSegF: row['mean [rating] of max-n-of 20 turtles with [group = "test"] [rating]'] - row['mean [rating] of max-n-of 20 turtles with [group = "segregated"] [rating]']
+
     });
 });
 
 });
 
 var numGroupsTop20 = 0;
+var numGroupsDiff = 0;
 const plotGroup = function (fractionW, segregationPref, group) {
     var runsToChart = Object.values(runs[fractionW][segregationPref]);
     // Add the lines
@@ -105,15 +127,49 @@ const plotGroup = function (fractionW, segregationPref, group) {
         .style("fill", "none")
         .attr("class", "series");        
 }
+const plotDiffGroup = function (fractionW, segregationPref, group) {
+    var runsToChart = Object.values(runs[fractionW][segregationPref]);
+    // Add the lines
+    var newColor = getNewDiffColor();
+    var line = d3.line()
+        .x(function (d) {            
+            return x(+d.step)
+        })
+        .y(function (d) {
+            return y(+d[group])
+        })
+    svgDiff.selectAll("myLines")
+        .data(runsToChart)
+        .enter()
+        .append("path")
+        .attr("d", function (d) {
+            return line(d.values)
+        })
+        .attr("stroke", function (d) {
+            return newColor;
+        })
+        .style("stroke-width", 1)
+        .style("fill", "none")
+        .attr("class", "series");        
+}
 
 const addNewSeries = function () 
 {
-    let fractionW = d3.select('input[name="fractionW"]:checked').node().value;
-    let segPreference = d3.select('input[name="segPreference"]:checked').node().value;
-    let groupToPlot = d3.select('input[name="groupToPlot"]:checked').node().value;
+    let fractionW = d3.select('.top20-dashboard input[name="fractionW"]:checked').node().value;
+    let segPreference = d3.select('.top20-dashboard input[name="segPreference"]:checked').node().value;
+    let groupToPlot = d3.select('.top20-dashboard input[name="groupToPlot"]:checked').node().value;
     plotGroup(fractionW, segPreference, groupToPlot);
     insertLegendItem(groupToPlot+" Part:"+fractionW+" Seg:"+segPreference,getNewColor());
     numGroupsTop20 ++;
+}
+const addNewDiffSeries = function () 
+{
+    let fractionW = d3.select('.diff-dashboard input[name="fractionW"]:checked').node().value;
+    let segPreference = d3.select('.diff-dashboard input[name="segPreference"]:checked').node().value;
+    let groupToPlot = d3.select('.diff-dashboard input[name="groupToPlot"]:checked').node().value;
+    plotDiffGroup(fractionW, segPreference, groupToPlot);
+    insertLegendItem(groupToPlot+" Part:"+fractionW+" Seg:"+segPreference,getNewDiffColor());
+    numGroupsDiff ++;
 }
 
 const clearPlot = function () 
@@ -127,10 +183,24 @@ const getNewColor = function ()
     index = numGroupsTop20 % colors.length;
     return colors[index];
 }
+const getNewDiffColor = function ()
+{
+    const colors = d3.schemeTableau10.concat(d3.schemeSet2);
+    index = numGroupsDiff % colors.length;
+    return colors[index];
+}
 
 const insertLegendItem = function(name, color) 
 {
     const legendItem = d3.select(".plot-legend").append("div").attr("class", "legend-item");
     legendItem.append("div").attr("class", "legend-color").style('background-color', color)
     legendItem.append("div").attr("class", "legend-text").text(name);
+}
+
+const selectDashboard = function(dashboard)
+{
+    d3.selectAll(".dashboard:not(."+dashboard+")")
+        .style("display", "none");
+    d3.select(".dashboard."+dashboard)
+        .style("display", "flex");
 }
